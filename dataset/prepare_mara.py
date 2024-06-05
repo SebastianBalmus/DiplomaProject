38b4ebda0ae51866b30d223caf2f314cbc42d0e4
#SebY #SebY <balmussebastian@gmail.com>
2024-06-05 18:37:59 +0300

    added docstrings for mara preparation

--- dataset/prepare_mara.py
+++ dataset/prepare_mara.py
@@ -4,20 +4,33 @@ import argparse
 
 
 def process_metadata(metadata_path, save_path):
+    """
+    Processes Mara dataset metadata by extracting filenames and retaining metadata,
+    then saves the processed data to a new CSV file.
+
+    Args:
+        metadata_path (str): Path to the input metadata CSV file.
+        save_path (str): Path where the processed metadata will be saved.
+
+    Note:
+        The input CSV is expected to be tab-delimited. The output CSV will be pipe-delimited.
+    """
     with open(metadata_path, "r", encoding="utf-8") as f:
         reader = csv.reader(f, delimiter="\t")
         data = list(reader)
-    
+
     processed_data = [
         [
-            p[0].split('/')[-1].split('.')[0],
+            p[0].split("/")[-1].split(".")[0],
             p[1],
-        ] for p in data
+        ]
+        for p in data
     ]
 
     with open(save_path, "w", encoding="utf-8") as f:
         writer = csv.writer(f, delimiter="|")
         writer.writerows(processed_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,7 +39,7 @@ if __name__ == "__main__":
         "-m",
         "--metadata_path",
         type=str,
-        default='/train_path/Mara/mara.csv',
+        default="/train_path/Mara/mara.csv",
         help="Path to metadata.csv",
     )
 
@@ -34,7 +47,7 @@ if __name__ == "__main__":
         "-s",
         "--save_path",
         type=str,
-        default='/train_path/Mara/metadata.csv',
+        default="/train_path/Mara/metadata.csv",
         help="Where to save processed metadata",
     )
 
