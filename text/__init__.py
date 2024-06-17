b4d273eb8460d60cbc89c3213b11e6ef189eb5ef
#SebY #SebY <balmussebastian@gmail.com>
2024-06-17 18:10:49 +0300

    multilang symbols

--- text/__init__.py
+++ text/__init__.py
@@ -1,12 +1,16 @@
 """ from https://github.com/keithito/tacotron """
 import re
 from text import cleaners
-from text.symbols import symbols
+from text.symbols import symbols, symbols_ro
 
 
 # Mappings from symbol to numeric ID and vice versa:
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
 _id_to_symbol = {i: s for i, s in enumerate(symbols)}
+
+# Mappings from symbol to numeric ID and vice versa for Romanian symbols:
+_symbol_to_id_ro = {s: i for i, s in enumerate(symbols_ro)}
+_id_to_symbol_ro = {i: s for i, s in enumerate(symbols_ro)}
 
 # Regular expression matching text enclosed in curly braces:
 _curly_re = re.compile(r'(.*?)\{(.+?)\}(.*)')
@@ -27,6 +31,8 @@ def text_to_sequence(text, cleaner_names):
   '''
   sequence = []
 
+  use_ro = cleaner_names[0] == "basic_cleaners"
+
   # Check for curly braces and treat their contents as ARPAbet:
   while len(text):
     m = _curly_re.match(text)
@@ -40,12 +46,17 @@ def text_to_sequence(text, cleaner_names):
   return sequence
 
 
-def sequence_to_text(sequence):
+def sequence_to_text(sequence, ro_chars):
   '''Converts a sequence of IDs back to a string'''
+  if ro_chars:
+    symbol_map = _id_to_symbol_ro
+  else:
+    symbol_map = _id_to_symbol
+
   result = ''
   for symbol_id in sequence:
-    if symbol_id in _id_to_symbol:
-      s = _id_to_symbol[symbol_id]
+    if symbol_id in symbol_map:
+      s = symbol_map[symbol_id]
       # Enclose ARPAbet back in curly braces:
       if len(s) > 1 and s[0] == '@':
         s = '{%s}' % s[1:]
@@ -62,13 +73,13 @@ def _clean_text(text, cleaner_names):
   return text
 
 
-def _symbols_to_sequence(symbols):
-  return [_symbol_to_id[s] for s in symbols if _should_keep_symbol(s)]
+def _symbols_to_sequence(symbols, use_ro=False):
+  _symbol_to_id_map = _symbol_to_id_ro if use_ro else _symbol_to_id
+  return [_symbol_to_id_map[s] for s in symbols if _should_keep_symbol(s, use_ro)]
 
+def _arpabet_to_sequence(text, use_ro=False):
+  return _symbols_to_sequence(['@' + s for s in text.split()], use_ro)
 
-def _arpabet_to_sequence(text):
-  return _symbols_to_sequence(['@' + s for s in text.split()])
-
-
-def _should_keep_symbol(s):
-  return s in _symbol_to_id and s != '_' and s != '~'
+def _should_keep_symbol(s, use_ro=False):
+  _symbol_to_id_map = _symbol_to_id_ro if use_ro else _symbol_to_id
+  return s in _symbol_to_id_map and s != '_' and s != '~'
